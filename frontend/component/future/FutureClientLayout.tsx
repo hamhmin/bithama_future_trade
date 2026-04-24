9dac23d1aa7e177a7611fe8741e588df49ce65eb
hamhmin <hamhmin7@naver.com>
2026-04-24 14:14:02 +0900

    future pc/mo 포폴용 완성도 ui 수정

--- frontend/component/future/FutureClientLayout.tsx
+++ frontend/component/future/FutureClientLayout.tsx
@@ -24,7 +24,7 @@ export default function FutureClientLayout({
   return (
     <>
       {/* 데스크탑 레이아웃 (기존 그대로) */}
-      <div className="hidden lg:grid h-screen bg-[#050d1a] text-white grid-cols-12 grid-rows-[48px_24px_1fr_200px]">
+      <div className="hidden lg:grid h-screen bg-[#050d1a] text-white grid-cols-12 grid-rows-[48px_24px_1fr_340px]">
         <SocketProvider />
         <div className="col-span-12 border-b border-gray-700">
           <FutureHeader initialTrade={trade} />
@@ -51,7 +51,7 @@ export default function FutureClientLayout({
       </div>
 
       {/* 모바일 레이아웃 */}
-      <div className="flex lg:hidden flex-col h-screen bg-[#050d1a] text-white">
+      <div className="flex lg:hidden flex-col   bg-[#050d1a] text-white h-full">
         <SocketProvider />
 
         {/* 헤더 */}
@@ -60,10 +60,7 @@ export default function FutureClientLayout({
         </div>
 
         {/* 상단 - 주문 + 호가창 */}
-        <div
-          className="flex border-b border-gray-700"
-          style={{ height: "60%" }}
-        >
+        <div className="flex border-b border-gray-700">
           {/* 좌: 주문패널 */}
           <div className="flex-1 min-w-0 border-r border-gray-700 overflow-y-auto">
             <OrderPanel />
